e89df298c121df3f470268cf8dd4a830194324ea
Eric Medvet <eric.medvet@gmail.com>
2020-08-18 12:34:22 +0200

    Initial commit

---
+++ src/main/java/it/units/erallab/Starter.java
@@ -0,0 +1,12 @@
+package it.units.erallab;
+
+/**
+ * @author eric
+ * @created 2020/08/18
+ * @project VSREvolution
+ */
+public class Starter {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
