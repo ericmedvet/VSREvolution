89238e571f6fae54e2d83a9cd521f22d70c19e29
Eric Medvet <eric.medvet@gmail.com>
2020-11-06 23:59:45 +0100

    Working on VideoMaker.java: now it assumes it receives a CSV with the grid coded as rows.

--- src/main/java/it/units/erallab/VideoMaker.java
+++ src/main/java/it/units/erallab/VideoMaker.java
@@ -16,7 +16,6 @@
 
 package it.units.erallab;
 
-import com.google.common.collect.Sets;
 import it.units.erallab.hmsrobots.core.objects.Robot;
 import it.units.erallab.hmsrobots.tasks.locomotion.Locomotion;
 import it.units.erallab.hmsrobots.util.Grid;
@@ -26,25 +25,19 @@ import it.units.erallab.hmsrobots.viewers.drawers.Ground;
 import it.units.erallab.hmsrobots.viewers.drawers.SensorReading;
 import it.units.erallab.hmsrobots.viewers.drawers.Voxel;
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.lang3.tuple.Pair;
 import org.dyn4j.dynamics.Settings;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.Arrays;
+import java.io.*;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.function.Predicate;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static it.units.malelab.jgea.core.util.Args.*;
 
@@ -55,89 +48,92 @@ import static it.units.malelab.jgea.core.util.Args.*;
  */
 public class VideoMaker {
 
-  private static final String PREDICATE_DEF = "≡";
-  private static final String PREDICATE_SEP = "\\^";
-  private static final String PREDICATE_F_SEP = ";";
-  private static final String[] PREDICATE_F_PARS = new String[]{"[", "]"};
-
   private static final Logger L = Logger.getLogger(VideoMaker.class.getName());
 
   /* example of invocation
     /usr/lib/jvm/jdk-14.0.1/bin/java -cp ~/IdeaProjects/VSREvolution/out/artifacts/VSREvolution_jar/VSREvolution.jar it.units.erallab.VideoMaker inputFile=vsrs-short-all-p10-t10-10.ser.txt globalPredicate=seed≡1^terrain≡uneven5^mapper≡centralized columnPredicates=evolver≡mlp-0-cmaes^body≡biped-4x3,evolver≡mlp-0-cmaes^body≡biped-cpg-4x3 rowPredicates=quant[births\;500\;2]≡0,quant[births\;500\;2]≡250,quant[births\;500\;2]≡500 transformation=identity
    */
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     //get params
-    String inputFileName = a(args, "inputFile", null);
+    String inputFileName = a(args, "inputFile", "/home/eric/experiments/vsrs-stat.txt");
     String outputFileName = a(args, "outputFile", null);
     String serializedRobotColumn = a(args, "serializedRobotColumnName", "best.serialized.robot");
     String terrainName = a(args, "terrain", "flat");
     String transformationName = a(args, "transformation", "identity");
-    double episodeTime = d(a(args, "episodeT", "30.0"));
+    double startTime = d(a(args, "startTime", "0.0"));
+    double endTime = d(a(args, "endTime", "30.0"));
     int w = i(a(args, "w", "1024"));
     int h = i(a(args, "h", "768"));
     int frameRate = i(a(args, "frameRate", "30"));
-    //read predicates
-    Map<String, Predicate<String>> globalPredicate = Arrays.stream(a(args, "globalPredicate", "").split(PREDICATE_SEP)).sequential()
-        .map(VideoMaker::buildPredicate)
-        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    List<Map<String, Predicate<String>>> columnPredicates = l(a(args, "columnPredicates", ""))
-        .stream()
-        .map(pairs -> Arrays.stream(pairs.split(PREDICATE_SEP)).sequential()
-            .map(VideoMaker::buildPredicate)
-            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)))
+    SerializationUtils.Mode mode = SerializationUtils.Mode.valueOf(a(args, "deserializationMode", SerializationUtils.Mode.GZIPPED_JSON.name()).toUpperCase());
+    //read data
+    Reader reader = null;
+    List<CSVRecord> records = null;
+    List<String> headers = null;
+    try {
+      if (inputFileName != null) {
+        reader = new FileReader(inputFileName);
+      } else {
+        reader = new InputStreamReader(System.in);
+      }
+      CSVParser csvParser = CSVFormat.DEFAULT.withDelimiter(';').withFirstRecordAsHeader().parse(reader);
+      records = csvParser.getRecords();
+      headers = csvParser.getHeaderNames();
+      reader.close();
+    } catch (IOException e) {
+      L.severe(String.format("Cannot read input data: %s", e));
+      if (reader != null) {
+        try {
+          reader.close();
+        } catch (IOException ioException) {
+          //ignore
+        }
+      }
+      System.exit(-1);
+    }
+    L.info(String.format("Read %d data lines from %s", records.size(), (inputFileName != null) ? inputFileName : "stdin"));
+    //TODO maybe check that col nums is appropriate
+    //find x- and y- values
+    String xHeader = headers.get(0);
+    String yHeader = headers.get(1);
+    List<String> xValues = records.stream()
+        .map(r -> r.get(xHeader))
+        .distinct()
         .collect(Collectors.toList());
-    List<Map<String, Predicate<String>>> rowPredicates = l(a(args, "rowPredicates", ""))
-        .stream()
-        .map(pairs -> Arrays.stream(pairs.split(PREDICATE_SEP)).sequential()
-            .map(VideoMaker::buildPredicate)
-            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)))
+    List<String> yValues = records.stream()
+        .map(r -> r.get(yHeader))
+        .distinct()
         .collect(Collectors.toList());
-    //read data from file
-    Reader reader = new FileReader(inputFileName);
-    List<Map<String, String>> records = CSVFormat.DEFAULT.withDelimiter(';').withFirstRecordAsHeader().parse(reader).getRecords().stream()
-        .map(CSVRecord::toMap).collect(Collectors.toList());
-    L.info(String.format("Read %d data lines from %s", records.size(), inputFileName));
-    //filter data
-    Grid<List<Map<String, String>>> recordGrid = Grid.create(
-        columnPredicates.size(),
-        rowPredicates.size(),
-        (x, y) -> records.stream()
-            .filter(r -> match(r, globalPredicate))
-            .filter(r -> match(r, columnPredicates.get(x)))
-            .filter(r -> match(r, rowPredicates.get(y)))
+    //build grid
+    List<CSVRecord> finalRecords = records;
+    Grid<List<String>> rawGrid = Grid.create(
+        xValues.size(),
+        yValues.size(),
+        (x, y) -> finalRecords.stream()
+            .filter(r -> r.get(xHeader).equals(xValues.get(x)) && r.get(yHeader).equals(yValues.get(y)))
+            .map(r -> r.get(2))
             .collect(Collectors.toList())
     );
-    L.info(String.format(
-        "Grid # of records: %s",
-        recordGrid.stream()
-            .map(e -> String.format("(%d,%d)->%d", e.getX(), e.getY(), e.getValue().size()))
-            .collect(Collectors.joining(", "))
-    ));
     //build named grid of robots
-    Set<String> relevantKeys = Stream.concat(columnPredicates.stream(), rowPredicates.stream())
-        .map(Map::keySet)
-        .reduce(Sets::union)
-        .orElse(Set.of());
     Grid<Pair<String, Robot<?>>> namedRobotGrid = Grid.create(
-        recordGrid,
-        r -> r.isEmpty() ? null : Pair.of(
-            r.get(0).entrySet().stream()
-                .filter(e -> relevantKeys.contains(e.getKey()))
-                .map(e -> e.toString())
-                .collect(Collectors.joining("\n")),
-            it.units.erallab.hmsrobots.util.Utils.buildRobotTransformation(transformationName).apply(SerializationUtils.deserialize(r.get(0).get(serializedRobotColumn), Robot.class, SerializationUtils.Mode.GZIPPED_JSON))
+        rawGrid.getW(),
+        rawGrid.getH(),
+        (x, y) -> Pair.of(
+            xValues.get(x) + " " + yValues.get(y),
+            it.units.erallab.hmsrobots.util.Utils.buildRobotTransformation(transformationName)
+                .apply(SerializationUtils.deserialize(rawGrid.get(x, y).get(0), Robot.class, mode))
         )
     );
     //prepare problem
     Locomotion locomotion = new Locomotion(
-        episodeTime,
+        endTime,
         Locomotion.createTerrain(terrainName),
         new Settings()
     );
     //do simulations
     ScheduledExecutorService uiExecutor = Executors.newScheduledThreadPool(4);
     ExecutorService executor = Executors.newCachedThreadPool();
-    GridSnapshotListener gridSnapshotListener;
+    GridSnapshotListener gridSnapshotListener = null;
     if (outputFileName == null) {
       gridSnapshotListener = new GridOnlineViewer(
           Grid.create(namedRobotGrid, Pair::getLeft),
@@ -145,21 +141,26 @@ public class VideoMaker {
       );
       ((GridOnlineViewer) gridSnapshotListener).start(3);
     } else {
-      gridSnapshotListener = new GridFileWriter(
-          w, h, frameRate,
-          new File(outputFileName),
-          Grid.create(namedRobotGrid, Pair::getLeft),
-          uiExecutor,
-          GraphicsDrawer.build().setConfigurable("drawers", List.of(
-              it.units.erallab.hmsrobots.viewers.drawers.Robot.build(),
-              Voxel.build(),
-              Ground.build(),
-              SensorReading.build()
-          )).setConfigurable("generalRenderingModes", Set.of(
-              GraphicsDrawer.GeneralRenderingMode.TIME_INFO,
-              GraphicsDrawer.GeneralRenderingMode.VOXEL_COMPOUND_CENTERS_INFO
-          ))
-      );
+      try {
+        gridSnapshotListener = new GridFileWriter(
+            w, h, frameRate,
+            new File(outputFileName),
+            Grid.create(namedRobotGrid, Pair::getLeft),
+            uiExecutor,
+            GraphicsDrawer.build().setConfigurable("drawers", List.of(
+                it.units.erallab.hmsrobots.viewers.drawers.Robot.build(),
+                Voxel.build(),
+                Ground.build(),
+                SensorReading.build()
+            )).setConfigurable("generalRenderingModes", Set.of(
+                GraphicsDrawer.GeneralRenderingMode.TIME_INFO,
+                GraphicsDrawer.GeneralRenderingMode.VOXEL_COMPOUND_CENTERS_INFO
+            ))
+        );
+      } catch (IOException e) {
+        L.severe(String.format("Cannot build grid file writer: %s", e));
+        System.exit(-1);
+      }
     }
     GridEpisodeRunner<Robot<?>> runner = new GridEpisodeRunner<>(
         namedRobotGrid,
@@ -172,38 +173,6 @@ public class VideoMaker {
       executor.shutdownNow();
       uiExecutor.shutdownNow();
     }
-  }
-
-  private static boolean match(Map<String, String> values, Map<String, Predicate<String>> predicates) {
-    for (String key : predicates.keySet()) {
-      if (!values.containsKey(key)) {
-        return false;
-      }
-      if (!predicates.get(key).test(values.get(key))) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  //expects a ^-piece of something like: evolver≡e1^body≡b1^quant(births|1000|2)≡3
-  private static Map.Entry<String, Predicate<String>> buildPredicate(String string) {
-    String left = string.split(PREDICATE_DEF)[0];
-    String right = string.split(PREDICATE_DEF)[1];
-    if (!left.contains(PREDICATE_F_PARS[0])) {
-      return Map.entry(left, s -> s.equals(right));
-    }
-    String fName = left.substring(0, left.indexOf(PREDICATE_F_PARS[0]));
-    String[] fArgs = left.substring(left.indexOf(PREDICATE_F_PARS[0]) + 1, left.indexOf(PREDICATE_F_PARS[1])).split(PREDICATE_F_SEP);
-    if (fName.equals("quant")) {
-      double range = Double.parseDouble(fArgs[1]);
-      int bins = Integer.parseInt(fArgs[2]);
-      return Map.entry(
-          fArgs[0],
-          s -> Math.floor(Double.parseDouble(s) / range * bins) * range / bins == Double.parseDouble(right)
-      );
-    }
-    return null;
   }
 
 }
