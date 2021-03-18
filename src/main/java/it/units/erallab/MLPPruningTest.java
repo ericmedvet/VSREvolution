f1ef0cffd34b85148fc144348b97556e8ae68444
Eric Medvet <eric.medvet@gmail.com>
2021-03-18 17:57:36 +0100

    Working on pruning test.

--- src/main/java/it/units/erallab/MLPPruningTest.java
+++ src/main/java/it/units/erallab/MLPPruningTest.java
@@ -22,10 +22,15 @@ import java.util.stream.IntStream;
 public class MLPPruningTest {
 
   public static void main(String[] args) throws IOException {
+    nnIOPlots();
+  }
+
+  public static void errPlots() throws IOException {
     int[] nOfInputs = {10, 25, 50, 100};
-    int[] nOfLayers = {1, 2, 3, 4};
+    int[] nOfLayers = {0, 1, 2};
     int[] seeds = IntStream.range(0, 30).toArray();
-    int n = 100;
+    double dT = 1d / 60d;
+    double totalT = 10d;
     double[] rates = IntStream.range(0, 20).mapToDouble(i -> (double) i * 0.025d).toArray();
     for (int nOfInput : nOfInputs) {
       for (int nOfLayer : nOfLayers) {
@@ -37,7 +42,7 @@ public class MLPPruningTest {
             names.add(context.name().toLowerCase() + "/" + criterion.name().toLowerCase());
           }
         }
-        Table<Double> t = new ArrayTable<>(names);
+        Table<Double> table = new ArrayTable<>(names);
         for (double rate : rates) {
           List<Double> row = new ArrayList<>();
           row.add(rate);
@@ -47,23 +52,23 @@ public class MLPPruningTest {
               for (int seed : seeds) {
                 Random r = new Random(seed);
                 MultiLayerPerceptron nn = new MultiLayerPerceptron(MultiLayerPerceptron.ActivationFunction.TANH, nOfInput, innerLayers, 1);
-                MultiLayerPerceptron pnn = new PruningMultiLayerPerceptron(MultiLayerPerceptron.ActivationFunction.TANH, nOfInput, innerLayers, 1, n / 2, context, criterion, rate);
+                MultiLayerPerceptron pnn = new PruningMultiLayerPerceptron(MultiLayerPerceptron.ActivationFunction.TANH, nOfInput, innerLayers, 1, totalT / 2, context, criterion, rate);
                 double[] ws = IntStream.range(0, nn.getParams().length).mapToDouble(i -> r.nextDouble() * 2d - 1d).toArray();
                 nn.setParams(ws);
                 pnn.setParams(ws);
-                for (int i = 0; i < n; i++) {
-                  final double finalI = i;
-                  double[] inputs = IntStream.range(0, nOfInput).mapToDouble(j -> Math.sin(finalI / (double) n * 10d / (double) (j + 1))).toArray();
+                for (double t = 0; t < totalT; t = t + dT) {
+                  final double finalT = t;
+                  double[] inputs = IntStream.range(0, nOfInput).mapToDouble(j -> Math.sin(finalT / (double) (j + 1))).toArray();
                   double localErr = Math.abs(nn.apply(inputs)[0] - pnn.apply(inputs)[0]);
-                  if (i > n / 2) {
+                  if (t > totalT / 2) {
                     err = err + localErr;
                   }
                 }
               }
-              row.add(err / (double) seeds.length / (double) n);
+              row.add(err / (double) seeds.length / totalT * 2d);
             }
           }
-          t.addRow(row);
+          table.addRow(row);
         }
         String fileName = String.format(
             "/home/eric/err-vs-rate-i%d-l%d.png",
@@ -72,7 +77,7 @@ public class MLPPruningTest {
         );
         System.out.println(fileName);
         ImageIO.write(
-            ImagePlotters.xyLines(800, 600).apply(t),
+            ImagePlotters.xyLines(800, 600).apply(table),
             "png",
             new File(fileName));
       }
@@ -82,12 +87,15 @@ public class MLPPruningTest {
   public static void nnIOPlots() throws IOException {
     int nOfInput = 2;
     int nOfCalls = 50;
+    double dT = 1d / 60d;
+    double totalT = 10d;
+    double pruningT = 5d;
     int[] innerLayers = new int[]{8, 7, 6};
     double[] rates = new double[]{0.05, 0.10, 0.15, 0.25, 0.33, 0.5};
     for (PruningMultiLayerPerceptron.Context context : PruningMultiLayerPerceptron.Context.values()) {
       for (PruningMultiLayerPerceptron.Criterion criterion : PruningMultiLayerPerceptron.Criterion.values()) {
         MultiLayerPerceptron nn = new MultiLayerPerceptron(MultiLayerPerceptron.ActivationFunction.TANH, nOfInput, innerLayers, 1);
-        List<PruningMultiLayerPerceptron> pnns = Arrays.stream(rates).mapToObj(r -> new PruningMultiLayerPerceptron(MultiLayerPerceptron.ActivationFunction.TANH, nOfInput, innerLayers, 1, nOfCalls, context, criterion, r)).collect(Collectors.toList());
+        List<PruningMultiLayerPerceptron> pnns = Arrays.stream(rates).mapToObj(r -> new PruningMultiLayerPerceptron(MultiLayerPerceptron.ActivationFunction.TANH, nOfInput, innerLayers, 1, pruningT, context, criterion, r)).collect(Collectors.toList());
         Random r = new Random(2);
         double[] ws = IntStream.range(0, nn.getParams().length).mapToDouble(i -> r.nextDouble() * 2d - 1d).toArray();
         nn.setParams(ws);
@@ -105,19 +113,19 @@ public class MLPPruningTest {
               rate
           ));
         }
-        Table<Double> t = new ArrayTable<>(names);
+        Table<Double> table = new ArrayTable<>(names);
 
-        for (double x = 0; x < 40; x = x + 0.1) {
-          double[] input = new double[]{Math.sin(0.1 * x), Math.sin(x)};
+        for (double t = 0; t < totalT; t = t + dT) {
+          double[] input = new double[]{Math.sin(0.1 * t), Math.sin(t)};
           List<Double> values = new ArrayList<>();
-          values.add(x);
+          values.add(t);
           values.add(nn.apply(input)[0]);
           pnns.forEach(pnn -> values.add(pnn.apply(input)[0]));
-          t.addRow(values);
+          table.addRow(values);
         }
 
         ImageIO.write(
-            ImagePlotters.xyLines(800, 600).apply(t),
+            ImagePlotters.xyLines(800, 600).apply(table),
             "png",
             new File(String.format(
                 "/home/eric/pnns-%s-%s.png",
