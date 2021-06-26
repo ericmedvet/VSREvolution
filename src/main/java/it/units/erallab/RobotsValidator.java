5d16c6dd020b3e046a936d820d5c30f4c62c77dd
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-06-26 19:49:13 +0200

    RobotsValidator updated to keep only useful original columns.

--- src/main/java/it/units/erallab/RobotsValidator.java
+++ src/main/java/it/units/erallab/RobotsValidator.java
@@ -3,6 +3,7 @@ package it.units.erallab;
 import it.units.erallab.hmsrobots.core.objects.Robot;
 import it.units.erallab.hmsrobots.tasks.locomotion.Locomotion;
 import it.units.erallab.hmsrobots.tasks.locomotion.Outcome;
+import it.units.erallab.hmsrobots.util.RobotUtils;
 import it.units.erallab.hmsrobots.util.SerializationUtils;
 import it.units.erallab.utils.Utils;
 import it.units.malelab.jgea.Worker;
@@ -16,12 +17,11 @@ import org.dyn4j.dynamics.Settings;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-import static it.units.malelab.jgea.core.util.Args.d;
-import static it.units.malelab.jgea.core.util.Args.l;
+import static it.units.malelab.jgea.core.util.Args.*;
 
 public class RobotsValidator extends Worker {
 
@@ -74,11 +74,15 @@ public class RobotsValidator extends Worker {
     String outputFileName = a("outputFile", "validation-redone.txt");
     double episodeTime = d(a("episodeTime", "10"));
     double episodeTransientTime = d(a("episodeTransientTime", "1"));
-    List<String> terrainNames = l(a("validationTerrain", "flat," +
+    List<String> validationTerrainNames = l(a("validationTerrain", "flat," +
         "hilly-1-10-0,hilly-1-10-1,hilly-1-10-2,hilly-1-10-3,hilly-1-10-4,hilly-1-10-5," +
         "steppy-1-10-0,steppy-1-10-1,steppy-1-10-2,steppy-1-10-3,steppy-1-10-4,steppy-1-10-5," +
         "downhill-10,downhill-20,uphill-10,uphill-20"));
+    int[] seeds = ri(a("seed", "0:1"));
+    List<String> validationTransformationNames = l(a("validationTransformation", "identity"));
     SerializationUtils.Mode mode = SerializationUtils.Mode.GZIPPED_JSON;
+    List<String> headersToKeep = List.of("iterations", "births", "fitness.evaluations", "elapsed.seconds", "experiment.name", "seed", "terrain",
+        "shape", "sensor.config", "mapper", "transformation", "evolver", "fitness.metrics");
     // create printer
     try {
       printer = new CSVPrinter(new PrintStream(outputFileName), CSVFormat.DEFAULT.withDelimiter(';'));
@@ -89,15 +93,16 @@ public class RobotsValidator extends Worker {
     // parse old file and print headers to new file
     List<String> oldHeaders = readRecordsFromFile(inputFileName);
     int serializedRobotColumnIndex = oldHeaders.indexOf(serializedRobotColumnName);
-    oldHeaders = oldHeaders.stream().filter(x -> !x.equals(serializedRobotColumnName)).collect(Collectors.toList());
+    oldHeaders = oldHeaders.stream().filter(h -> headersToKeep.contains(h)).collect(Collectors.toList());
     List<NamedFunction<Outcome, ?>> basicOutcomeFunctions = Utils.basicOutcomeFunctions();
     List<NamedFunction<Outcome, ?>> detailedOutcomeFunctions = Utils.detailedOutcomeFunctions(spectrumMinFreq, spectrumMaxFreq, spectrumSize);
     List<String> basicOutcomeFunctionsNames = basicOutcomeFunctions.stream().map(NamedFunction::getName).collect(Collectors.toList());
     List<String> detailedOutcomeFunctionsNames = detailedOutcomeFunctions.stream().map(NamedFunction::getName).collect(Collectors.toList());
-    List<String> headers = new ArrayList<>(oldHeaders);
-    headers.add("validationTerrain");
-    headers.addAll(basicOutcomeFunctionsNames);
-    headers.addAll(detailedOutcomeFunctionsNames);
+    List<String> headers = new ArrayList<>(oldHeaders.stream().map(h -> "event." + h).collect(Collectors.toList()));
+    headers.addAll(List.of("keys.validation.transformation", "keys.validation.seed", "keys.validation.terrain"));
+    headers.addAll(basicOutcomeFunctionsNames.stream().map(h -> "outcome." + h).collect(Collectors.toList()));
+    headers.addAll(detailedOutcomeFunctionsNames.stream().map(h -> "outcome." + h).collect(Collectors.toList()));
+
     try {
       printer.printRecord(headers);
     } catch (IOException e) {
@@ -108,26 +113,34 @@ public class RobotsValidator extends Worker {
       // read robot and record
       Robot<?> robot = SerializationUtils.deserialize(record.get(serializedRobotColumnIndex), Robot.class, mode);
       robot.reset();
-      List<String> oldRecord = IntStream.range(0, record.size()).filter(index -> index != serializedRobotColumnIndex).mapToObj(
-          record::get).collect(Collectors.toList());
-      // validate robot on all terrains
-      List<List<Object>> rows = terrainNames.stream().parallel()
-          .map(terrainName -> {
-            List<Object> cells = new ArrayList<>(oldRecord);
-            cells.add(terrainName);
-            Locomotion locomotion = new Locomotion(episodeTime, Locomotion.createTerrain(terrainName), new Settings());
-            Outcome outcome = locomotion.apply(SerializationUtils.clone(robot));
-            cells.addAll(basicOutcomeFunctions.stream().map(f -> f.apply(outcome.subOutcome(episodeTransientTime, episodeTime))).collect(Collectors.toList()));
-            cells.addAll(detailedOutcomeFunctions.stream().map(f -> f.apply(outcome.subOutcome(episodeTransientTime, episodeTime))).collect(Collectors.toList()));
-            return cells;
-          }).collect(Collectors.toList());
-      rows.forEach(row -> {
-        try {
-          printer.printRecord(row);
-        } catch (IOException e) {
-          e.printStackTrace();
+      List<String> oldRecord = oldHeaders.stream().map(h -> record.get(h)).collect(Collectors.toList());
+
+      for (String validationTransformationName : validationTransformationNames) {
+        for (int seed : seeds) {
+          Random random = new Random(seed);
+          robot = SerializationUtils.clone(robot, SerializationUtils.Mode.GZIPPED_JSON);
+          Robot<?> transformedRobot = RobotUtils.buildRobotTransformation(validationTransformationName, random).apply(robot);
+          // validate robot on all terrains
+          List<List<Object>> rows = validationTerrainNames.stream().parallel()
+              .map(terrainName -> {
+                List<Object> cells = new ArrayList<>(oldRecord);
+                cells.addAll(List.of(validationTransformationName, seed, terrainName));
+                Locomotion locomotion = new Locomotion(episodeTime, Locomotion.createTerrain(terrainName), new Settings());
+                Outcome outcome = locomotion.apply(SerializationUtils.clone(transformedRobot));
+                cells.addAll(basicOutcomeFunctions.stream().map(f -> f.apply(outcome.subOutcome(episodeTransientTime, episodeTime))).collect(Collectors.toList()));
+                cells.addAll(detailedOutcomeFunctions.stream().map(f -> f.apply(outcome.subOutcome(episodeTransientTime, episodeTime))).collect(Collectors.toList()));
+                return cells;
+              }).collect(Collectors.toList());
+          rows.forEach(row -> {
+            try {
+              printer.printRecord(row);
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          });
         }
-      });
+      }
+
       logger.info(String.format("%2d/%2d", ++validationsCounter, records.size()));
     }
     // close printer
