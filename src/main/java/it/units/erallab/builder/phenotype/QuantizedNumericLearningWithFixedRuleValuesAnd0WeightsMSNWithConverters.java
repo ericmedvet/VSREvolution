490119b904a788d4dd7507fcea71e544c86a570f
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-09-20 16:54:43 +0200

    New learning rules evolver.

--- src/main/java/it/units/erallab/builder/phenotype/QuantizedNumericLearningWithFixedRuleValuesAnd0WeightsMSNWithConverters.java
+++ src/main/java/it/units/erallab/builder/phenotype/QuantizedNumericLearningWithFixedRuleValuesAnd0WeightsMSNWithConverters.java
@@ -4,7 +4,10 @@ import it.units.erallab.builder.PrototypedFunctionBuilder;
 import it.units.erallab.hmsrobots.core.controllers.MultiLayerPerceptron;
 import it.units.erallab.hmsrobots.core.controllers.TimedRealFunction;
 import it.units.erallab.hmsrobots.core.controllers.snn.learning.STDPLearningRule;
-import it.units.erallab.hmsrobots.core.controllers.snndiscr.*;
+import it.units.erallab.hmsrobots.core.controllers.snndiscr.QuantizedLearningMultilayerSpikingNetwork;
+import it.units.erallab.hmsrobots.core.controllers.snndiscr.QuantizedMultilayerSpikingNetwork;
+import it.units.erallab.hmsrobots.core.controllers.snndiscr.QuantizedMultilayerSpikingNetworkWithConverters;
+import it.units.erallab.hmsrobots.core.controllers.snndiscr.QuantizedSpikingFunction;
 import it.units.erallab.hmsrobots.core.controllers.snndiscr.converters.stv.QuantizedSpikeTrainToValueConverter;
 import it.units.erallab.hmsrobots.core.controllers.snndiscr.converters.vts.QuantizedValueToSpikeTrainConverter;
 
@@ -12,21 +15,26 @@ import java.util.Collections;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
-public class QuantizedNumericLearningMSNWithConverters implements PrototypedFunctionBuilder<List<Double>, TimedRealFunction> {
+public class QuantizedNumericLearningWithFixedRuleValuesAnd0WeightsMSNWithConverters implements PrototypedFunctionBuilder<List<Double>, TimedRealFunction> {
 
   private final double innerLayerRatio;
   private final int nOfInnerLayers;
   private final BiFunction<Integer, Integer, QuantizedSpikingFunction> neuronBuilder;
   private final QuantizedValueToSpikeTrainConverter valueToSpikeTrainConverter;
   private final QuantizedSpikeTrainToValueConverter spikeTrainToValueConverter;
+  private final double[] defaultSymmetricParams;
+  private final double[] defaultAsymmetricParams;
 
-  public QuantizedNumericLearningMSNWithConverters(double innerLayerRatio, int nOfInnerLayers, BiFunction<Integer, Integer, QuantizedSpikingFunction> neuronBuilder, QuantizedValueToSpikeTrainConverter valueToSpikeTrainConverter, QuantizedSpikeTrainToValueConverter spikeTrainToValueConverter) {
+  public QuantizedNumericLearningWithFixedRuleValuesAnd0WeightsMSNWithConverters(double innerLayerRatio, int nOfInnerLayers, BiFunction<Integer, Integer, QuantizedSpikingFunction> neuronBuilder, QuantizedValueToSpikeTrainConverter valueToSpikeTrainConverter, QuantizedSpikeTrainToValueConverter spikeTrainToValueConverter, double[] defaultSymmetricParams, double[] defaultAsymmetricParams) {
     this.innerLayerRatio = innerLayerRatio;
     this.nOfInnerLayers = nOfInnerLayers;
     this.neuronBuilder = neuronBuilder;
     this.valueToSpikeTrainConverter = valueToSpikeTrainConverter;
     this.spikeTrainToValueConverter = spikeTrainToValueConverter;
+    this.defaultSymmetricParams = defaultSymmetricParams;
+    this.defaultAsymmetricParams = defaultAsymmetricParams;
   }
 
   private int[] innerNeurons(int nOfInputs, int nOfOutputs) {
@@ -52,26 +60,17 @@ public class QuantizedNumericLearningMSNWithConverters implements PrototypedFunc
       int nOfOutputs = function.getOutputDimension();
       int[] innerNeurons = innerNeurons(nOfInputs, nOfOutputs);
       int nOfWeights = QuantizedMultilayerSpikingNetwork.countWeights(nOfInputs, innerNeurons, nOfOutputs);
-      if (7 * nOfWeights != values.size()) {
+      if (2 * nOfWeights != values.size()) {
         throw new IllegalArgumentException(String.format(
             "Wrong number of values for learning rules: %d expected, %d found",
-            7 * nOfWeights,
+            2 * nOfWeights,
             values.size()
         ));
       }
-      double[] weights = values.subList(0, values.size() / 7).stream().mapToDouble(d -> d).toArray();
-      List<Double> flatRulesGenerator = values.subList(values.size() / 7, values.size());
-      double[][] rulesGenerator = new double[flatRulesGenerator.size() / 6][6];
-      int j = 0;
-      for (int i = 0; i < flatRulesGenerator.size(); i++) {
-        int pos = i % 6;
-        rulesGenerator[j][pos] = flatRulesGenerator.get(i);
-        if (pos == 5) {
-          j++;
-        }
-      }
-      STDPLearningRule[] flatLearningRules = STDPLearningRule.createLearningRules(rulesGenerator);
-      QuantizedLearningMultilayerSpikingNetwork quantizedLearningMultilayerSpikingNetwork = new QuantizedLearningMultilayerSpikingNetwork(nOfInputs, innerNeurons, nOfOutputs,weights, flatLearningRules, neuronBuilder);
+      double[] weights = new double[nOfWeights];
+      double[][] rulesGenerator = IntStream.range(0, values.size() / 2).map(i -> 2 * i).mapToObj(i -> new double[]{values.get(i), values.get(i + 1)}).toArray(double[][]::new);
+      STDPLearningRule[] flatLearningRules = STDPLearningRule.createLearningRules(rulesGenerator, defaultSymmetricParams, defaultAsymmetricParams);
+      QuantizedLearningMultilayerSpikingNetwork quantizedLearningMultilayerSpikingNetwork = new QuantizedLearningMultilayerSpikingNetwork(nOfInputs, innerNeurons, nOfOutputs, weights, flatLearningRules, neuronBuilder);
       return new QuantizedMultilayerSpikingNetworkWithConverters<>(
           quantizedLearningMultilayerSpikingNetwork,
           valueToSpikeTrainConverter,
@@ -83,7 +82,7 @@ public class QuantizedNumericLearningMSNWithConverters implements PrototypedFunc
   @Override
   public List<Double> exampleFor(TimedRealFunction function) {
     return Collections.nCopies(
-        7 * QuantizedMultilayerSpikingNetwork.countWeights(
+        2 * QuantizedMultilayerSpikingNetwork.countWeights(
             MultiLayerPerceptron.countNeurons(
                 function.getInputDimension(),
                 innerNeurons(function.getInputDimension(), function.getOutputDimension()),
